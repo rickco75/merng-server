16f4901580012c42d806eca689ab3ef4de364a73
Rick Cohen <rickco@gmail.com>
2020-10-25 13:46:00 -0500

    initial commit

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const { MONGODB } = require('./config.js');
 
 const pubSub = new PubSub();
 
+const PORT = process.env.port || 5000;
+
 const server = new ApolloServer({
     typeDefs,
     resolvers,
@@ -16,10 +18,13 @@ const server = new ApolloServer({
 mongoose.connect(MONGODB, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log('MongoDB Connected')
-        return server.listen({ port: 5000 })
+        return server.listen({ port: PORT })
         .then(res => {
             console.log(`Server running at ${res.url}`)
         })
-    });
+    })
+    .catch(err => {
+      console.error(err)
+    })
 
 
--- util/checkAuth.js
+++ util/checkAuth.js
@@ -5,7 +5,6 @@ const { SECRET_KEY } = require('../config');
 
 
 module.exports = (context) => {
-    //console.log(context)
     const authHeader = context.req.headers.authorization;
     if (authHeader){
         // Bearer ...
